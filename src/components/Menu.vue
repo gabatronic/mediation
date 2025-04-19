86e7b11fe254b198c5aaa249a3371efabeab7f21
Gaba <miguel.gabaldon@vocali.net>
2025-04-19 09:35:59 +0200

    Fix menu on mobile and add burger click

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,14 +1,19 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import MenuItem from './MenuItem.vue'
 import BurgerMenuIcon from './Icons/BurgerMenuIcon.vue'
+const isOpen = ref<boolean>(false)
+const toggleMenu = () => {
+    isOpen.value = !isOpen.value    
+}
 </script>
 <template>
-<div class="w-full relative">
-    <div class=" text-white p-2 flex flex-row sm:gap-2 w-full  py-3 relative container mx-auto">
+<div class="w-full relative sm:flex sm:flex-row sm:container">
+    <div class="text-white p-2 flex flex-row sm:gap-2 py-3 relative  mx-auto">
         <img src="../assets//images/logo.png" alt="Logo" class=" left-4 top-3 h-10" />
-        <BurgerMenuIcon class="stroke-white w-16 absolute right-0 " />
+        <BurgerMenuIcon class="stroke-white w-10 absolute sm:hidden right-10 " @click="toggleMenu"/>
     </div>
-    <div class="flex flex-col sm:flex-row gap-4  overflow-y-hidden sm:gap-1 absolute right-2 top-2">
+    <div class="bg-background flex flex-col sm:flex-row gap-4 transition-all overflow-y-hidden sm:gap-1 absolute sm:relative right-1 top-2 h-0 sm:h-fit" :class="{'h-0 sm:h-fit': !isOpen, 'h-fit left-0 translate-y-12 py-4': isOpen}">
             <MenuItem to="/" icon="Inicio" label="Inicio" class="mt-2" />
             <MenuItem to="/process" icon="info" label="Proceso" class="mt-2" />
             <MenuItem to="/warranties" icon="info" label="Garantias" class="mt-2" />
@@ -16,7 +21,7 @@ import BurgerMenuIcon from './Icons/BurgerMenuIcon.vue'
             <MenuItem to="/lawyers" icon="envelope" label="Para abogados" class="mt-2"/>
             <MenuItem to="/faq" icon="info" label="FAQ" class="mt-2"/>
             <MenuItem to="/client-area" icon="info" label="Acceder" class="mt-2"/>
-            <MenuItem to="/form" icon="info" label="Iniciar Mediacion" class="bg-gray-800 pr-3 pl-2 py-2 rounded-md" />
+            <MenuItem to="/form" icon="info" label="Iniciar Mediacion" class="rounded-md" />
         </div>    
 </div>    
 </template>
